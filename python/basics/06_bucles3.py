3b6381fd641635ec529f7b01b2837bee409f33a7
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-14 19:55:52 -0500

    clase 2025-10-14

--- python/basics/06_bucles3.py
+++ python/basics/06_bucles3.py
@@ -1,3 +1,12 @@
 """
 WHILE
+
+Pide numero de emlpeados
+cada empleado solicita nombre y salario
+determina quien tiene el mayor salario yb  muestralo
 """
+
+num = int(input("ingrese el número de empleados"))
+
+for i in range(num):
+    print("hola")
